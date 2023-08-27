052b44b441d82b443ec1f3523bda4620274ba6b3
mbojanks <bmilosavljevic@gmail.com>
2023-08-27 18:50:57 +0200

    inlang I18n setup

---
+++ inlang.config.js
@@ -0,0 +1,24 @@
+/**
+* @type { import("@inlang/core/config").DefineConfig }
+*/
+export async function defineConfig(env) {
+	const { default: jsonPlugin } = await env.$import('https://cdn.jsdelivr.net/npm/@inlang/plugin-json@3/dist/index.js');
+	const { default: sdkPlugin } = await env.$import(
+		"https://cdn.jsdelivr.net/npm/@inlang/sdk-js-plugin/dist/index.js",
+	)
+
+	const { default: standardLintRules } = await env.$import('https://cdn.jsdelivr.net/npm/@inlang/plugin-standard-lint-rules@3/dist/index.js');
+
+	return {
+		referenceLanguage: 'en',
+		plugins: [
+			jsonPlugin({ pathPattern: 'lang/{language}.json' }),
+			standardLintRules(),
+			sdkPlugin({
+				languageNegotiation: {
+					strategies: [{ type: "localStorage" }],
+				},
+			}),
+		],
+	};
+}
